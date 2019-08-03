f5994b3dff10bf24e5fd1f226371624b87cabd5a
thiaguh11 <thiagosilva110299@gmail.com>
2019-08-03 20:17:36 -0300

    Adding All Files

---
+++ src/Components/Display.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Display.css'
+
+export default props => 
+        <>
+            <div className="display2">{props.resValue}</div>
+            <div className="display1">{props.value}</div>
+        </>
